910a38e5acdd22c06c09c59a2dad4f45b0233e7d
KMH8359 <50608712+KMH8359@users.noreply.github.com>
2024-04-16 13:46:17 +0900

    basic_01 실습

--- APISERVER/WeatherForecast.cs
+++ APISERVER/WeatherForecast.cs
@@ -1,12 +1,15 @@
-namespace TodoApi;
+using System;
 
-public class WeatherForecast
+namespace APISERVER
 {
-    public DateOnly Date { get; set; }
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
 
-    public int TemperatureC { get; set; }
+        public int TemperatureC { get; set; }
 
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-    public string? Summary { get; set; }
+        public string? Summary { get; set; }
+    }
 }
