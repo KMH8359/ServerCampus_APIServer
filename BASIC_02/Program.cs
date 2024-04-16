8fbd3cffe88ccd39befc91efacd9a1a699a15c10
KMH8359 <50608712+KMH8359@users.noreply.github.com>
2024-04-16 16:04:35 +0900

    실습 폴더 정리
    코드 분석하기

--- BASIC_02/Program.cs
+++ BASIC_02/Program.cs
@@ -1,15 +1,10 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
