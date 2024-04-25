ccf06be5528e924f4ab875409d7db53ae8436fd1
KMH8359 <50608712+KMH8359@users.noreply.github.com>
2024-04-25 18:36:04 +0900

    ADD GAMESERVER FOLDER

---
+++ GAMESERVER/CSharp_OmokClient_1/CSCommon/PacketDatas.cs
@@ -0,0 +1,164 @@
+﻿using MemoryPack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSCommon
+{
+    [MemoryPackable]
+    public partial class PKTResponse : MemoryPackPacketHead
+    {
+        public short Result { get; set; }
+    }
+
+
+
+    // 로그인 요청
+    [MemoryPackable]
+    public partial class PKTReqLogin : MemoryPackPacketHead
+    {
+        public string UserID { get; set; }
+        public string AuthToken { get; set; }
+    }
+
+    [MemoryPackable]
+    public partial class PKTResLogin : PKTResponse
+    {
+        
+    }
+
+
+
+    [MemoryPackable]
+    public partial class PKTNtfMustClose : PKTResponse
+    {
+    }
+
+
+
+    [MemoryPackable]
+    public partial class PKTReqRoomEnter : MemoryPackPacketHead
+    {
+        public int RoomNum { get; set; }
+    }
+     
+    [MemoryPackable]
+    public partial class PKTResRoomEnter : PKTResponse
+    {
+    }
+
+    [MemoryPackable]
+    public partial class PKTNtfRoomUserList : MemoryPackPacketHead
+    {
+        public List<string> UserIDList { get; set; } = new List<string>();
+    }
+
+    [MemoryPackable]
+    public partial class PKTNtfRoomNewUser : MemoryPackPacketHead
+    {
+        public string UserID { get; set; }
+    }
+
+
+    [MemoryPackable]
+    public partial class PKTReqRoomLeave : MemoryPackPacketHead
+    {
+    }
+
+    [MemoryPackable]
+    public partial class PKTResRoomLeave : PKTResponse
+    {
+    }
+
+    [MemoryPackable]
+    public partial class PKTNtfRoomLeaveUser : MemoryPackPacketHead
+    {
+        public string UserID { get; set; }
+    }
+
+
+    [MemoryPackable]
+    public partial class PKTReqRoomChat : MemoryPackPacketHead
+    {
+        public string ChatMessage { get; set; }
+    }
+
+    [MemoryPackable]
+    public partial class PKTResRoomChat : PKTResponse
+    {
+    }
+
+    [MemoryPackable]
+    public partial class PKTNtfRoomChat : MemoryPackPacketHead
+    {
+        public string UserID;
+
+        public string ChatMessage;
+    }
+
+    [MemoryPackable]
+    public partial class PKTInternalReqRoomEnter : MemoryPackPacketHead
+    {
+        public int RoomNumber;
+
+        public string UserID;
+    }
+
+
+    // 오목 플레이 준비 완료 요청
+    [MemoryPackable]
+    public partial class PKTReqReadyOmok : PKTResponse
+    {
+    }
+
+    [MemoryPackable]
+    public partial class PKTResReadyOmok : PKTResponse
+    {
+    }
+
+    [MemoryPackable]
+    public partial class PKTNtfReadyOmok : MemoryPackPacketHead
+    {
+        public string UserID;
+        public bool IsReady;
+    }
+
+
+    // 오목 시작 통보(서버에서 클라이언트들에게)
+    [MemoryPackable]
+    public partial class PKTNtfStartOmok : MemoryPackPacketHead
+    {
+        public string FirstUserID; // 선턴 유저 ID
+    }
+
+
+    // 돌 두기
+    [MemoryPackable]
+    public partial class PKTReqPutMok : MemoryPackPacketHead
+    {
+        public int PosX;
+        public int PosY;        
+    }
+
+    [MemoryPackable]
+    public partial class PKTResPutMok : PKTResponse
+    {
+    }
+
+    [MemoryPackable]
+    public partial class PKTNtfPutMok : MemoryPackPacketHead
+    {
+        public int PosX;
+        public int PosY;
+        public int Mok;
+    }
+
+    // 오목 게임 종료 통보
+    [MemoryPackable]
+    public partial class PKTNtfEndOmok : MemoryPackPacketHead
+    {
+        public string WinUserID;
+    }
+
+   
+}
---
+++ GAMESERVER/CSharp_OmokClient_1/CSCommon/PacketHeader.cs
@@ -0,0 +1,83 @@
+﻿using MemoryPack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSCommon
+{
+    public struct MemoryPackPacketHeaderInfo
+    {
+        const int PacketHeaderMemoryPackStartPos = 3;
+        public const int HeadSize = 8;
+
+        public UInt16 TotalSize;
+        public UInt16 ID;
+        public byte Type;
+
+        public static UInt16 GetTotalSize(byte[] data, int startPos)
+        {
+            return FastBinaryRead.UInt16(data, startPos + PacketHeaderMemoryPackStartPos);
+        }
+                
+        public static void Write(byte[] data, UInt16 totalSize, UInt16 packetID)
+        {
+            // FastBinaryWrite.UInt16(data, PacketHeaderMemoryPackStartPos, totalSize);
+            FastBinaryWrite.UInt16(data, PacketHeaderMemoryPackStartPos + 2, packetID);
+        }
+
+
+        public void Read(byte[] headerData)
+        {
+            var pos = PacketHeaderMemoryPackStartPos;
+
+            TotalSize = FastBinaryRead.UInt16(headerData, pos);
+            pos += 2;
+
+            ID = FastBinaryRead.UInt16(headerData, pos);
+            pos += 2;
+
+            Type = headerData[pos];
+            pos += 1;
+        }
+
+        public void Write(byte[] packetData)
+        {
+            var pos = PacketHeaderMemoryPackStartPos;
+
+            FastBinaryWrite.UInt16(packetData, pos, TotalSize);
+            pos += 2;
+
+            FastBinaryWrite.UInt16(packetData, pos, ID);
+            pos += 2;
+
+            packetData[pos] = Type;
+            pos += 1;
+        }
+
+        public byte[] Write()
+        {
+            var packetData = new byte[HeadSize];
+            var pos = PacketHeaderMemoryPackStartPos;
+
+            FastBinaryWrite.UInt16(packetData, pos, TotalSize);
+            pos += 2;
+
+            FastBinaryWrite.UInt16(packetData, pos, ID);
+            pos += 2;
+
+            packetData[pos] = Type;
+            pos += 1;
+
+            return packetData;
+        }
+    }
+
+
+    [MemoryPackable]
+    public partial class MemoryPackPacketHead
+    {
+        public UInt16 TotalSize { get; set; } = 0;
+        public UInt16 Id { get; set; } = 0;
+        public byte Type { get; set; } = 0;
+    }
+}
